b216ea87061f0a6687e6750b32449c8cffa1b92c
rrhurtado <rrhurtado@uc.cl>
2016-09-11 11:51:11 -0300

    =capistrano production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '52.33.15.147', user: 'deploy', roles: %w{web app}
+server '52.33.15.147', user: 'ubuntu', roles: %w{web app}
 
 
 # server-based syntax
